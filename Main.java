dbc70db6f1d35ccc79486dcb961186260e752357
ioddeech <118965058+ioddeech@users.noreply.github.com>
2022-12-13 10:05:32 +0300

    10:05 13/12/2022 Редакцтирование?

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Basket vasyaBasket = new Basket(100);
-        vasyaBasket.add("Молоко", 1.89);
+        vasyaBasket.add("Молоко", 1.89, 3);
         vasyaBasket.add("Батон", 0.99);
 
         Basket petyaBasket = new Basket(500);
@@ -10,8 +10,8 @@ public class Main {
 
         Basket olgaBasket = new Basket();
 
-        olgaBasket.add("Стол", 500, 2);
-        olgaBasket.add("Ваза мексиканская", 829.99);
+        olgaBasket.add("Стол", 554.99, 2);
+        olgaBasket.add("Ваза мексиканская", 829.19);
 
         vasyaBasket.print("Корзина Василия: ");
         petyaBasket.print("Корзина Петра: ");
